60b9b1660815471d4da449df3c6b761b9c29c7a3
RadoslawG123 <gorecki208@onet.pl>
2025-09-08 21:44:27 +0200

    Initial Commit

---
+++ Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryConsoleApp
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public bool Available { get; set; }
+        public decimal Price { get; private set; }
+
+        public Book(string title) 
+        {
+            Title = title;
+            Price = 0;
+
+            Available = true;
+        }
+
+        private string setPrice(string title, decimal price) 
+        {
+            decimal newPrice;
+            if (price < 0)
+                return "Price shouldn't be lower than 0$. Please try again.";
+
+            return "Price is setted successfully for '" + title + "'";
+        }
+    }
+}
---
+++ Library.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryConsoleApp
+{
+    internal class Library
+    {
+        public static void Main()
+        {
+            int input = -1;
+            List<Book> Books = [];
+
+            while (true) 
+            {
+                Console.WriteLine("""
+                
+                Welcome to the library!
+
+                Options:
+                1. Show all Books
+                2. Add new Book
+                3. Delete Book
+                4. Borrow Book
+                5. Exit
+
+                Choice: 
+                """);
+
+                try
+                {
+                    input = int.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("""
+                        --------------------------------------------------------------
+                        Incorrect choice!
+                        You can type a choice as a single digit between numbers: 1-5.
+                        Please try again.
+                        --------------------------------------------------------------
+                        """);
+                } 
+
+                if (input == 1)
+                {
+                    Console.WriteLine("""
+                        All books in the library:
+
+                        """);
+                    foreach(Book book in Books)
+                    {
+
+                    }
+                }
+                if(input == 5)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+            }
+        }
+    }
+}
